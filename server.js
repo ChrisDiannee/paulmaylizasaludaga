2797ab0e0ad5702231e1a96553b0b8aa89a9674f
ChrisDiannee <cdcalamba02.cdc@gmail.com>
2017-07-25 23:02:20 +0800

    my first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use(express.static(path.join(__dirname, 'public/')));
+
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/SEO.HTML'));
+});
+app.get('/PART2.HTML', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/PART2.HTML'));
+});
+app.get('/PART3.HTML', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/PART3.HTML'));
+});
+app.get('/PART4.HTML', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/PART4.HTML'));
+});
+app.get('/BACKLINKS.HTML', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/BACKLINKS.HTML'));
+});
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
